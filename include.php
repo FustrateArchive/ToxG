ecb0dc6e250b28ed67913335cd36a4e4292a3e75
Dragooon <mail@dragooon.net>
2011-01-07 20:39:04 +0530

    + Added an include.php, automatically includes all files.

---
+++ include.php
@@ -0,0 +1,15 @@
+<?php
+// These might normally be __autoload()'d, or similar.
+require_once('include/toxgbuilder.php');
+require_once('include/toxgsource.php');
+require_once('include/toxgparser.php');
+require_once('include/toxgstandardelements.php');
+require_once('include/toxgerrors.php');
+require_once('include/toxgexception.php');
+require_once('include/toxgexpression.php');
+require_once('include/toxgoverlay.php');
+require_once('include/toxgtemplate.php');
+require_once('include/toxgtemplatelist.php');
+require_once('include/toxgtoken.php');
+require_once('include/toxgprebuilder.php');
+?>
