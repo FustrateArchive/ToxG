7a5f52302b8ba50c3bc61288db343c0793e14df3
Dragooon <mail@dragooon.net>
2011-01-08 23:43:37 +0530

    + Added toxg.php, base class for TOX-G and this is what which should be used for including Tox-G. Based on SampleToxgTheme created by [Unknown] modified by Fustrate but further enhanced. Basically I've added support for overlay calls, and implemented better caching of files(It now caches files based on the file's name and overlay calls).

---
+++ toxg.php
@@ -0,0 +1,168 @@
+<?php
+
+require_once('include.php');
+
+class Toxg
+{
+	protected $nsuri = 'urn:site:template';
+	protected $extension = 'tpl';
+	protected $template_dir = '.';
+	protected $inherited_dirs = array();
+	protected $compile_dir = '.';
+	protected $mtime_check = true;
+	protected $needs_compile = false;
+
+	protected $overlays = array();
+	protected $mtime = 0;
+
+	protected $namespaces = array();
+
+	protected $templates = null;
+	protected $layers = array();
+	protected $inside = array();
+	protected $template_params = array();
+	protected $compiled_list = array();
+	protected $_templates = array();
+	protected $_overlays = array();
+
+	public function __construct($template_dir, $compile_dir, array $inherited_dirs = array(), $needs_compile = true)
+	{
+		if (!is_null($needs_compile) && is_bool($needs_compile))
+			$this->needs_compile = $needs_compile;
+
+		$this->template_dir = $template_dir;
+		$this->compile_dir = $compile_dir;
+		$this->inherited_dirs = $inherited_dirs;
+
+		$this->templates = new ToxgTemplateList();
+
+		$this->namespaces = array(
+			'sidebar' => 'urn:ns:site',
+			'site' => $this->nsuri,
+			'tpl' => ToxgTemplate::TPL_NAMESPACE,
+		);
+	}
+
+	public function setTemplateParam($key, $value)
+	{
+		$this->template_params[$key] = $value;
+	}
+
+	public function listenEmit($name, $callback)
+	{
+		return $this->templates->listenEmitBasic($name, $callback);
+	}
+
+	public function callOverlays(array $name, array $ns)
+	{
+		return $this->templates->callOverlays($name, $ns);
+	}
+
+	public function loadOverlay($filename)
+	{
+		$this->_overlays[] = $filename;
+	}
+
+	public function loadTemplates($filename)
+	{
+		$this->_templates[] = $filename;
+	}
+
+	public function addLayer($name, $namespace = 'site')
+	{
+		$this->layers[] = array($name, $namespace);
+	}
+
+	public function resetLayers()
+	{
+		$this->layers = array();
+	}
+
+	public function recompile()
+	{
+		$this->needs_compile = true;
+	}
+
+	public function addTemplate($name, $ns = 'site')
+	{
+		$this->inside[] = array($name, $ns);
+	}
+
+	public function resetTemplates($name)
+	{
+		$this->inside = array();
+	}
+
+	public function addNamespace($name, $nsuri = null)
+	{
+		$nsuri = is_null($nsuri) ? 'urn:ns:' . $name : $nsuri;
+
+		$this->namespaces[$name] = $nsuri;
+	}
+
+	public function output()
+	{
+		$overlay_hash = substr(md5('dummy' . implode('', $this->_overlays)), 0, 25);
+
+		foreach ($this->_templates as $filename)
+		{
+			$source = $this->template_dir . '/' . $filename . '.' . $this->extension;
+			$compiled = $this->compile_dir . '/toxg.' . $filename . '.' . $overlay_hash . '.php';
+
+			$inherited = array();
+			foreach ($this->inherited_dirs as $dir)
+				$inherited[] = $dir . '/' . $filename . '.tpl';
+
+			// Note: if overlays change, this won't work unless the overlay was touched.
+			// Normally, you'd flush the system when it needs a recompile.
+			if ($this->mtime_check && !$this->needs_compile)
+			{
+				$this->mtime = max($this->mtime, filemtime($source));
+
+				foreach ($inherited as $file)
+					$this->mtime = max($this->mtime, filemtime($file));
+
+				$this->needs_compile = !file_exists($compiled) || filemtime($compiled) <= $this->mtime;
+			}
+
+			$this->templates->addTemplate($source, $compiled, $inherited);
+		}
+
+		foreach ($this->_overlays as $filename)
+		{
+			$full = $this->template_dir . '/' . $filename . '.tpl';
+
+			$this->mtime = max($this->mtime, filemtime($full));
+			$this->templates->addOverlays(array($full));
+		}
+
+		if ($this->needs_compile)
+		{
+			ToxgStandardElements::useIn($this->templates);
+			$this->templates->setNamespaces($this->namespaces);
+			$this->templates->compileAll();
+		}
+
+		$this->templates->loadAll();
+
+		foreach ($this->layers as $layer)
+			$this->callTemplate($layer[0], 'above', $layer[1]);
+
+		foreach ($this->inside as $inside)
+		{
+			$this->callTemplate($inside[0], 'above', $inside[1]);
+			$this->callTemplate($inside[0], 'below', $inside[1]);
+		}
+
+		$reversed = array_reverse($this->layers);
+		foreach ($reversed as $layer)
+			$this->callTemplate($layer[0], 'below', $layer[1]);
+	}
+
+	protected function callTemplate($name, $side, $nsuri = 'site')
+	{
+		$func = ToxgExpression::makeTemplateName($this->namespaces[$nsuri], $name . '--toxg-direct') . '_' . $side;
+		$func($this->template_params);
+	}
+}
+?>
