be8b3260ad4bfb3ea4ff13d63a719aff1f5f5247
Dragooon <mail@dragooon.net>
2011-01-08 18:12:28 +0530

    !- Fixed line endings messup

--- include/toxgexception.php
+++ include/toxgexception.php
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class ToxgException extends Exception
-{
-	public $tpl_file = null;
-	public $tpl_line = 0;
-
-	public function __construct($message, $file, $line)
-	{
-		$this->tpl_file = $file;
-		$this->tpl_line = $line;
-
-		parent::__construct($file . '[' . $line . '] ' . $message, 0);
-	}
-}
-
-=======
 <?php
 
 class ToxgException extends Exception
@@ -38,6 +20,4 @@ class ToxgException extends Exception
 		parent::__construct($file . '[' . $line . '] ' . $message . '<br /><br /><pre>Backtrace:<br />' . implode('<br />', $list) . '</pre>', 0);
 	}
 }
-
->>>>>>> dragooon
 ?>
