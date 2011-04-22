31d0114f2484816c9b97043d7bce5fe372a68eee
Steven Hoffman <fustrate@me.com>
2011-04-21 17:48:43 -0700

    ! Enable $context for samples. Might change in the actual code in the future.

--- samples/sampletoxgtheme.php
+++ samples/sampletoxgtheme.php
@@ -2,12 +2,20 @@
 
 class SampleToxgTheme extends ToxgTheme
 {
+	public $context = array();
+
 	public function isTemplateUsed($name)
 	{
 		if ($this->needs_load)
 			throw new ToxgException('untranslated', 'Templates haven\'t been loaded yet, call loadAll() first.');
 
 		return ToxgTemplate::isTemplateUsed($this->nsuri, $name);
+	}
+
+	public function output()
+	{
+		$this->addCommonVars($this->context);
+		parent::output();
 	}
 }
 
