7f97f9cf14f8d4506fb52b217c6c2aa06a894711
Steven Hoffman <fustrate@me.com>
2011-04-25 11:20:39 -0700

    ! Give all theme sample themes their own compile dir so they don't double parse

--- samples/themes/theme.php
+++ samples/themes/theme.php
@@ -9,7 +9,7 @@ class MyTheme extends SampleToxgTheme
 	{
 		$this->theme = $name;
 		$this->template_dir = dirname(__FILE__) . '/themes/' . $name;
-		$this->compile_dir = dirname(__FILE__) . '/';
+		$this->compile_dir = dirname(__FILE__) . '/' . $name . '/';
 		$this->inherited_dirs[] = $name === 'base' ? array() : (dirname(__FILE__) . '/themes/base');
 
 		parent::__construct($this->template_dir, $this->compile_dir, $this->inherited_dirs);
